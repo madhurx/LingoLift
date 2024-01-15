9ea8e64e4b9e54a447b2b660663eee7c3a701ab3
Madhur Mittal <madhurmittal55@gmail.com>
2024-01-15 12:10:49 +0530

    feat: Home Page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import { Button, Container, Stack, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+const languages = [
+	{
+		name: "Japanese",
+		code: "ja",
+	},
+	{
+		name: "Hindi",
+		code: "hi",
+	},
+	{
+		name: "Spanish",
+		code: "es",
+	},
+	{
+		name: "French",
+		code: "fr",
+	},
+];
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+	const navigate = useNavigate();
+	const langSelectionHandler = (language: string): void => {
+		navigate(`/learning?language=${language}`);
+	};
 
-export default Home
+	return (
+		<Container maxWidth={"sm"}>
+			<Typography variant={"h3"} p={"2rem"} textAlign={"center"}>
+				Welcome
+			</Typography>
+			<Stack
+				direction={"row"}
+				spacing={"2rem"}
+				p={"2rem"}
+				alignItems={"center"}
+				justifyContent={"center"}>
+				{languages.map((i) => {
+					return (
+						<Button
+							key={i.code}
+							variant="contained"
+							onClick={() => {
+								langSelectionHandler(i.code);
+							}}>
+							{i.name}
+						</Button>
+					);
+				})}
+			</Stack>
+			<Typography textAlign={"center"}>
+				Choose one language from above
+			</Typography>
+		</Container>
+	);
+};
+
+export default Home;
