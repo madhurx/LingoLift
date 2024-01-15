945093d17b719aff3b929ffea980d66f004b00c6
Madhur <madhurrgupta@gmail.com>
2024-01-15 07:42:39 +0530

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { lime, purple } from "@mui/material/colors";
+
+const theme = createTheme({
+	palette: {
+		primary: lime,
+		secondary: purple,
+	},
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider theme={theme}>
+			<CssBaseline>
+				<App />
+			</CssBaseline>
+		</ThemeProvider>
 	</React.StrictMode>,
 );
