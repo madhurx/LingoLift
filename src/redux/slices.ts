3477f422ff3a9c8daf2c884c356a1b90773b068f
Madhur Mittal <madhurmittal55@gmail.com>
2024-01-16 12:09:53 +0530

    feat: reducer

--- src/redux/slices.ts
+++ src/redux/slices.ts
@@ -1,9 +1,46 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+const initialState: StateType = {
+	loading: false,
+	result: [],
+	error: undefined,
+	words: [],
+};
 
 const rootSlice = createSlice({
 	name: "root",
-	initialState: {},
-	reducers: {},
+	initialState,
+	reducers: {
+		getWordsRequest: (state) => {
+			state.loading = true;
+		},
+		getWordsSuccess: (state, action: PayloadAction<WordType[]>) => {
+			state.loading = false;
+			state.words = action.payload;
+		},
+		getWordsFail: (state, action: PayloadAction<string>) => {
+			state.loading = false;
+			state.error = action.payload;
+		},
+		saveResult: (state, action: PayloadAction<string[]>) => {
+			state.loading = false;
+			state.result = action.payload;
+		},
+		clearState: (state) => {
+			state.loading = false;
+			state.result = [];
+			state.words = [];
+			state.error = undefined;
+		},
+	},
 });
+
+export const {
+	getWordsRequest,
+	getWordsSuccess,
+	getWordsFail,
+	saveResult,
+	clearState,
+} = rootSlice.actions;
 
 export default rootSlice.reducer;
