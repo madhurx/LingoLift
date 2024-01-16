adc43dbd68e8c81723244b9f963b68896daaef76
Madhur Mittal <madhurmittal55@gmail.com>
2024-01-16 10:05:04 +0530

    feat: configure store

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
-import { lime, purple } from "@mui/material/colors";
+import { purple } from "@mui/material/colors";
 import "./main.css";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
 
 const theme = createTheme({
 	palette: {
@@ -16,7 +18,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<ThemeProvider theme={theme}>
 			<CssBaseline>
-				<App />
+				<Provider store={store}>
+					<App />
+				</Provider>
 			</CssBaseline>
 		</ThemeProvider>
 	</React.StrictMode>,
